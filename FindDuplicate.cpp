febda0fdc660ec7813dac1d2b5d2643ad10ac975
harshk461 <hk2152573@gmail.com>
2022-11-20 23:38:34 +0530

    new add

---
+++ FindDuplicate.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(int *arr, int size)
+{
+    if (size == 0)
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    int arr[6] = {1, 2, 3, 3, 4, 5};
+    int size = 6;
+    return 0;
+}
