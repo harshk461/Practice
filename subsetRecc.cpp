23db201e462bcd5cbead08a8287dbf6080899348
harshk461 <hk2152573@gmail.com>
2022-11-30 00:14:24 +0530

    new adds

---
+++ subsetRecc.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans)
+{
+    // base case
+    if (index >= nums.size())
+    {
+        ans.push_back(output);
+        return;
+    }
+
+    // exclude
+    solve(nums, output, index + 1, ans);
+
+    // include
+    int element = nums[index];
+    output.push_back(element);
+    solve(nums, output, index + 1, ans);
+}
+
+vector<vector<int>> subsets(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    vector<int> output;
+    int index = 0;
+    solve(nums, output, index, ans);
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 2, 3};
+    vector<vector<int>> ans = subsets(arr);
+    cout << ans.size();
+    return 0;
+}
