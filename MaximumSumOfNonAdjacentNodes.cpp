ae552930e1493d0a54dda010fe158beaad3e96c5
Harsh <hk2152573@gmail.com>
2023-03-27 23:37:22 +0530

    commit

---
+++ MaximumSumOfNonAdjacentNodes.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to return the maximum sum of non-adjacent nodes.
+    pair<int, int> solve(Node *root)
+    {
+        // base case
+        if (root == NULL)
+        {
+            pair<int, int> p = make_pair(0, 0);
+            return p;
+        }
+
+        pair<int, int> left = solve(root->left);
+        pair<int, int> right = solve(root->right);
+
+        pair<int, int> res;
+        res.first = root->data + left.second + right.second;
+        res.second - max(left.first, left.second) + max(right.first, right.second);
+
+        return res;
+    }
+
+    int getMaxSum(Node *root)
+    {
+        pair<int, int> ans = solve(root);
+        return max(ans.first, ans.second);
+    }
+};
+int main()
+{
+
+    return 0;
+}
