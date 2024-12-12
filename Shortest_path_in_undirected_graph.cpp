8d36ad4aad5df7b61e1e99e09f40867c4673d46e
harshk461 <hk2152573@gmail.com>
2024-12-12 19:12:12 +0530

    Resolve case conflict in filenames

--- Shortest_path_in_undirected_graph.cpp
+++ Shortest_path_in_undirected_graph.cpp
@@ -1,59 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TC-O(N+E) SC-O(N+E)
-vector<int> shortestPath(vector<pair<int, int>> edges, int n, int m, int s, int t)
+#pragma GCC target("avx, mmx, sse2, sse3, sse4")
+
+static const int disableSync = [](void) noexcept -> int
 {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
 
-    // create adjancey matrix
-    unordered_map<int, list<int>> adj;
-    for (int i = 0; i < edges.size(); i++)
+class Solution
+{
+public:
+    vector<int> shortestPath(vector<vector<int>> &edges, int N, int M, int src)
     {
-        int u = edges[i][0];
-        int v = edges[i][1];
-
-        adj[u].push_back(v);
-        adj[v].push_back(u);
     }
-
-    unordered_map<int, bool> visited;
-    unordered_map<int, int> parent;
-    queue<int> q;
-
-    q.push(s);
-    visited[s] = true;
-    parent[s] = -1;
-
-    // do bfs
-    while (!q.empty())
-    {
-        int front = q.front();
-        q.pop();
-
-        for (auto i : adj[front])
-        {
-            if (!visited[i])
-            {
-                visited[i] = true;
-                parent[i] = front;
-                q.push(i);
-            }
-        }
-    }
-
-    // prepare shortest path
-    vector<int> ans;
-    int currNode = t;
-    ans.push_back(t);
-    while (currNode != s)
-    {
-        currNode = parent[currNode];
-        ans.push_back(currNode);
-    }
-
-    reverse(ans.begin(), ans.end());
-    return ans;
-}
+};
 
 int main()
 {
